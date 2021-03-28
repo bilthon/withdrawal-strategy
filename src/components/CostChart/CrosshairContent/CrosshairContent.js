41de413ecde2579faff67aecdf1a97078a00b091
Nelson R. Perez <bilthon@gmail.com>
2021-03-28 14:32:33 -0500

    feature: adds localization to the CrosshairContent component

--- src/components/CostChart/CrosshairContent/CrosshairContent.js
+++ src/components/CostChart/CrosshairContent/CrosshairContent.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { FormattedMessage, useIntl } from 'react-intl';
 import './CrosshairContent.css';
 
 const CrosshairContent = ({ crosshairValues, formatValue, getWithdrawalCost, unit}) => {
 	let content = null;
+
+	const intl = useIntl();
 
 	const _formatValue = value => {
 		let formatted;
@@ -12,16 +15,45 @@ const CrosshairContent = ({ crosshairValues, formatValue, getWithdrawalCost, uni
 		return formatted;
 	}
 
+	const _getTitle = () => intl.formatMessage({
+		id: 'app.costChart.crosshair.title',
+		defaultMessage: 'Withdrawal'
+	});
+
+	const _getSavings = (savings) => intl.formatMessage({
+		id: 'app.costChart.crosshair.saving',
+		defaultMessage: `Savings : ${_formatValue(savings)}`,
+	}, { saving: _formatValue(savings) })
+
 	if (crosshairValues.length > 0) {
 		const { y } = crosshairValues[0];
 		const savings = getWithdrawalCost() - y;
 		content = (
 			<div className='CrossHair'>
-				<h4 className='CrossHairTitle'>Withdrawal</h4>
-				<p className='CrossHairField'>Amount: {_formatValue(crosshairValues[0].x)} </p>
-				<p className='CrossHairField'>Direct Cost : {_formatValue(getWithdrawalCost())} </p>
-				<p className='CrossHairField'>Indirect Cost : {_formatValue(crosshairValues[0].y)} </p>
-				<p className='CrossHairField' style={savings < 0 ? {color: '#FFE7E7', fontSize: 'bold'} : {}}>Savings : {_formatValue(savings)} </p>
+				<h4 className='CrossHairTitle'>{_getTitle()}</h4>
+				<FormattedMessage
+					className='CrossHairField'
+					id="app.costChart.crosshair.amount"
+					values={{ amount: _formatValue(crosshairValues[0].x) }}
+				/>
+				<br/>
+				<FormattedMessage
+					id="app.costChart.crosshair.direct"
+					default={`Direct Cost : ${_formatValue(getWithdrawalCost())}`}
+					values={{ directCost: _formatValue(getWithdrawalCost()) }}
+				/>
+				<br/>
+				<FormattedMessage
+					id="app.costChart.crosshair.indirect"
+					default={`Indirect Cost : ${_formatValue(crosshairValues[0].y)}`}
+					values={{ indirectCost: _formatValue(crosshairValues[0].y) }}
+				/>
+				<br/>
+				<p
+					className='CrossHairField'
+					style={savings < 0 ? {color: '#FFE7E7', fontSize: 'bold'} : {}}>
+					{ _getSavings(savings) }
+				</p>
 			</div>
 		);
 	}
